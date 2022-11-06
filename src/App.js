440b9e82ec99871f75e28285f9722f83dd10935f
rareprince <bankson03@gmail.com>
2022-11-07 00:25:21 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,82 +1,105 @@
-import React, {useState} from "react";
+import React, { useState, useRef } from "react";
 import "./style.css";
-import useForm from './component/form';
-//import ErrorBoundary from './component/error';
-//import {Routes, Route} from 'react-router-dom';
-import Layout from './component/layout';
-import NavBar from './component/Route';
+import { Routes, Route } from "react-router-dom";
+import useForm from "./component/form";
+import ErrorBoundary from "./component/error";
+import Repository from "./component/repository";
+//import Layout from './component/layout';
+//import NavBar from './component/Route';
 
-const Form =({handleSubmit, handleChange, input})=> {
-return (
-  <section>
-    <div className="form">
-      <h1 className="header">GITHUB PORTFOLIO</h1>
-      <p className="para">
-        Enter your Github username to access your portfolio.
-      </p>
-      <form className="username" ref={FormRef} onSubmit={handleSubmit}>
-        <label htmlFor="github" className="label">
-          Username:
-        </label>
-        <input
-          className="input"
-          id="github"
-          type="text"
-          name="github"
-          placeholder="e.g. rareprince"
-          onChange={handleChange}
-          value={input.github}
-        />
-        {gitHubUser.map((username)=>{
-          return(<a href={`https://github.com/${username}`} target="_blank"> {username}</a>)
-        })}
-      </form>
-    </div>
-  </section>
-)
-}
-
-const DisplayGitHubUser = ({gitHubUser})=>{
-  if (gitHubUser?.length===0) {
-    return<div>Kindly input your username</div>
-  }
-  return(<div>
-    value={searchParams.get('filter')||''}
-    onChange={(event)=>{
-      let filter =event.target.value;
-      if (filter){
-        setSearchParams({filter});
-      } else {
-        setSearchParams({});
-      }
-    }}
-{gitHubUser.filter((username)=>{
-  let fil
-})}
-    </div>)
-}
-
-
-
-export default function App() {
+function App() {
   const [gitHubUser, setGitHubUser] = useState([]);
-  const { resetForm, input, handleChange } = useForm({ github: '' });
-  
+  const { resetForm, input, handleChange } = useForm({ github: "" });
+  const formRef = useRef(null);
   const handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault();}
 
-    setGitHubUser((prev) => {
-      return [...prev, input.github];});
-      resetForm();
+  const Form = ({ handleSubmit, handleChange, input }) => {};
+  const username = githubInput.value;  
+  const DisplayGitHubUser = ({ gitHubUser }) => {
+    if (gitHubUser?.length === 0) {
+      return <div>Kindly input your username</div>;
+    }
+    return (
+      <div>
+        value={searchParams.get("filter") || ""}
+        onChange=
+        {(event) => {
+          let filter = event.target.value;
+          if (filter) {
+            setSearchParams({ filter });
+          } else {
+            setSearchParams({});
+          }
+        }}
+        
+        {gitHubUser
+          .filter((username) => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+            let name = username.toLowerCase();
+            return 
+            name.includes(filter.toLowerCase());
+          })
+          .map((usename) => {
+            <DisplayGitHubUser username={username} />;
+          })}
+      </div>
+    );
   };
-  const [isActive, setIsActive] = useState(false);
 
+  setGitHubUser((prev) => {
+        return [...prev, input.github];
+  });
+  resetForm();
 
   return (
     <section>
-      <Layout />
-      <Form/>
-      <NavBar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="repository" element={<Repository />} />
+          <Route
+            path="*"
+            element={
+              <div>
+                <h1>404 Page</h1>
+                <p>Sorry the page you are looking for does not exist</p>
+              </div>
+            }
+          />
+        </Routes>
+        ;
+      </div>
+      <div className="form">
+        <h1 className="header">GITHUB PORTFOLIO</h1>
+        <p className="para">
+          Enter your Github username to access your portfolio.
+        </p>
+        <form className="username" ref={formRef} onSubmit={handleSubmit}>
+          <label htmlFor="github" className="label">
+            Username:
+          </label>
+          <input
+            className="input"
+            id="github"
+            type="text"
+            name="github"
+            placeholder="e.g. rareprince"
+            onChange={handleChange}
+            value={input.github}
+          />
+          {gitHubUser.map((username) => {
+            return (
+              <a href={`https://github.com/${username}`} target="_blank">
+                {" "}
+                {username}
+              </a>
+            );
+          })}
+        </form>
+      </div>
     </section>
   );
 }
+export default App;
--- src/component/Route.js
+++ src/component/Route.js
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+/*import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 //let Form = lazy(() => import('./component/form'));
